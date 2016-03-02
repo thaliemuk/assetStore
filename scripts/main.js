5d0a8daeca756dc17baf7283903a0ad7b424be2f
tali <thalie.mukhtar@timetoknow.com>
2016-03-02 11:22:29 +0200

    some more changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,79 +3,109 @@ $(document).ready(function () {
        var data = {
             galleryData :[
                {
-                    "thumbnail" : "css/media/1.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (1).jpg",
+                    "name": "1",
+                    "source":"t2k",
+                    "price": null,
+                    "tags":"bloop,chart"
+
+               },{
+                    "thumbnail" :"css/media/IMAGES/1 (2).jpg",
+                    "name": "2",
+                    "source":"powtoon",
+                    "price":"5.99$",
+                    "tags":"animal,bloop,chart,balloon"
+
+               },{
+                    "thumbnail" :"css/media/IMAGES/1 (3).jpg",
+                    "name": "3",
+                    "source":"teachpitch",
+                    "price":null,
+                    "tags":"animal,bloop,chart,balloon"
+
+               },{
+                    "thumbnail" :"css/media/IMAGES/1 (4).jpg",
                     "name": "1",
                     "source":"check123",
+                    "price":"5.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/2.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (5).jpg",
                     "name": "2",
-                    "source":"check123",
+                    "source":"powtoon",
+                    "price":"5.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/3.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (6).jpg",
                     "name": "3",
-                    "source":"check123",
+                    "source":"t2k",
+                    "price":"2.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/1.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (7).jpg",
                     "name": "1",
                     "source":"check123",
+                    "price":"5.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/2.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (8).jpg",
                     "name": "2",
-                    "source":"check123",
+                    "source":"teachpitch",
+                    "price":"0.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/3.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (9).jpg",
                     "name": "3",
                     "source":"check123",
+                    "price":"5.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/1.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (10).jpg",
                     "name": "1",
-                    "source":"check123",
+                    "source":"powtoon",
+                    "price":"5.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/2.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (11).jpg",
                     "name": "2",
-                    "source":"check123",
+                    "source":"teachpitch",
+                    "price":null,
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/3.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (12).jpg",
                     "name": "3",
                     "source":"check123",
+                    "price":"10.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/1.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (13).jpg",
                     "name": "1",
-                    "source":"check123",
+                    "source":"t2k",
+                    "price":"5.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/2.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (14).jpg",
                     "name": "2",
-                    "source":"check123",
+                    "source":"teachpitch",
+                    "price":"4.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                },{
-                    "thumbnail" : "css/media/3.jpg",
+                    "thumbnail" :"css/media/IMAGES/1 (15).jpg",
                     "name": "3",
-                    "source":"check123",
-
-               },{
-                    "thumbnail" : "css/media/1.jpg",
-                    "name": "1",
-                    "source":"check123",
-
-               },{
-                    "thumbnail" : "css/media/2.jpg",
-                    "name": "2",
-                    "source":"check123",
-
-               },{
-                    "thumbnail" : "css/media/3.jpg",
-                    "name": "3",
-                    "source":"check123",
+                    "source":"t2k",
+                    "price":"3.99$",
+                    "tags":"animal,bloop,chart,balloon"
 
                }
 
@@ -84,11 +114,16 @@ $(document).ready(function () {
 
 	   var galleryHtml = Mustache.render(template,data);
 	   $("#gallery").html(galleryHtml);
-});
+    });
 
 	$(".serachIcon").on('click',function(){
 		var textToSearch = $(".searchBox-text").text();
 	});
+
+    setInterval(function(){ 
+        var imageToDisplayIndex = Math.floor(Math.random() * 6) + 1 ; 
+        $(".background").css({'background-image':'url("css/media/backgrounds/HP_0'+imageToDisplayIndex+'.jpg")'});
+    }, 3000);
 });
 
 	
